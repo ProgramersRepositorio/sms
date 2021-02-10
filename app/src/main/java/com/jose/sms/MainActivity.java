35b92f7ffa29272ed9142cc2a123718be4a9a0f9
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-02-10 11:35:59 +0100

    Initial commit

---
+++ app/src/main/java/com/jose/sms/MainActivity.java
@@ -0,0 +1,58 @@
+package com.jose.sms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+        ImageButton btnEnviar;
+        TextView txtTelefono, txtMensaje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEnviar = findViewById(R.id.buttonEnviarSms);
+        txtTelefono = findViewById(R.id.textTelefono);
+        txtMensaje = findViewById(R.id.textMensaje);
+
+
+        if(ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED&& ActivityCompat.checkSelfPermission(
+                MainActivity.this,Manifest
+                        .permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]
+                    { Manifest.permission.SEND_SMS,},1000);
+        }
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarMensaje(txtTelefono.getText().toString(), txtMensaje.getText().toString());
+            }
+        });
+    }
+    private void enviarMensaje (String numero, String mensaje){
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(numero,null,mensaje,null,null);
+            Toast.makeText(getApplicationContext(), "Mensaje Enviado.", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Mensaje no enviado, datos incorrectos.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+}
