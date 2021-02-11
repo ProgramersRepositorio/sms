d76164362796cb9d7fa8772e818a92728e702050
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-02-11 10:14:29 +0100

    Corregidos pequeños errores

--- app/src/main/java/com/jose/sms/MainActivity.java
+++ app/src/main/java/com/jose/sms/MainActivity.java
@@ -34,42 +34,31 @@ public class MainActivity extends AppCompatActivity {
         txtPrecision = findViewById(R.id.textPrecision);
         txtAltura = findViewById(R.id.textAltitud);
 
-        //comprobamos si tenemos los permisos necesarios
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-
-        if(ActivityCompat.checkSelfPermission(
-                MainActivity.this, Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                MainActivity.this,Manifest
-                        .permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]
-                    { Manifest.permission.SEND_SMS,},1000);
-        }
-
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String mensaje = obtenerLocalizacion();
-                enviarMensaje(txtTelefono.getText().toString(),mensaje);
+                enviarMensaje(txtTelefono.getText().toString(), mensaje);
             }
         });
     }
-    private void enviarMensaje (String numero, String mensaje){
+
+    private void enviarMensaje(String numero, String mensaje) {
+        //comprobamos si tenemos los permisos necesarios
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest
+                        .permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                    {Manifest.permission.SEND_SMS,}, 1000);
+        }
         try {
             SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(numero,null,mensaje,null,null);
+            sms.sendTextMessage(numero, null, mensaje, null, null);
             Toast.makeText(getApplicationContext(), "Mensaje Enviado.", Toast.LENGTH_LONG).show();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Mensaje no enviado, datos incorrectos.", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
@@ -77,18 +66,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String obtenerLocalizacion() {
-        String texto ="";
+        String texto = "";
+
+        //comprobamos si tenemos los permisos necesarios
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this,
                         Manifest.permission.ACCESS_COARSE_LOCATION) !=
                         PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(MainActivity.this,
-                    "No tiene los permisos necesarios",
-                    Toast.LENGTH_SHORT);
-            //return;
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
+
         localizador = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         localizacion = localizador.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         txtLatitud.setText(String.valueOf(localizacion.getLatitude()));
